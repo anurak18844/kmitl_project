9ea09c048baf003f70e9d5f0f60e0b1a891fec7e
anurak18844 <anurak18844@gmail.com>
2024-10-01 02:50:59 +0700

    add speech recognition

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from crf_model import resp_from_model as resp_model
-import json
+import speech_recognition as sr
+import pyaudio
 if __name__ == "__main__":
-    txt = "หมึกผัดไขา่ของโต๊ะ 4 กับ 7 เตรียมให้แล้วหรือยัง"
-    result = resp_model.predict_resp(txt)
-    print(result)
+    
+
+    sr.Microphone.list_microphone_names()
+    mic = sr.Microphone(1)
+    recog = sr.Recognizer()
+
+    with mic as source:
+        audio = recog.listen(source)
+
+
+    with mic as source:
+        while True:
+            audio = recog.listen(source)
+            try:
+                print("Listen..")
+                txt = recog.recognize_google(audio,language='th')
+                print(txt)
+                # txt = "หมึกผัดไขา่ของโต๊ะ 4 กับ 7 เตรียมให้แล้วหรือยัง"
+                result = resp_model.predict_resp(txt)
+                print(result)
+                
+            except:
+                continue
+    
